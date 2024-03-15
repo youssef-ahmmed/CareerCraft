5da33c19fbb6a79f2534ba88909ce2d4b2232b0f
Salma Hussien <salmaa.hussien1@gmail.com>
2024-03-15 23:41:29 +0200

    feat[backend]: implement user dao

---
+++ src/models/dao/user.dao.ts
@@ -0,0 +1,57 @@
+import bcrypt from 'bcrypt';
+
+import prisma from './client.db';
+import IUser from '../../types/IUser';
+
+class UserDao {
+  static async createUser(userDto: any) {
+    userDto.password = bcrypt.hashSync(userDto.password, Number(process.env.SECRET));
+    return prisma.users.create({
+      data: userDto,
+    });
+  }
+
+  static async getUserByEmail(userDto: IUser) {
+    return prisma.users.findUnique({
+      where: {
+        email: userDto.email,
+      },
+    });
+  }
+
+  static async getUserById(userDto: IUser) {
+    return prisma.users.findUnique({
+      where: {id: userDto.id},
+      include: {
+        notifications: true,
+        reviews: true,
+        skills: true,
+        jobs: true,
+      },
+    });
+  }
+
+  static async updateUserById(userDto: IUser) {
+    return prisma.users.update({
+      where: {id: userDto.id},
+      data: userDto,
+    });
+  }
+
+  static async getUserJobs(userDto: IUser) {
+    const user = await UserDao.getUserById(userDto);
+    return user.jobs;
+  }
+
+  static async getUserSkills(userDto: IUser) {
+    const user = await UserDao.getUserById(userDto);
+    return user.skills;
+  }
+
+  static async getUserNotifications(userDto: IUser) {
+    const user = await UserDao.getUserById(userDto);
+    return user.notifications;
+  }
+}
+
+export default UserDao;
