5f6c091d8c1a69840a185efd0c64deefba26aab9
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-15 15:37:22 +0200

    feat[backend]: Implement `DTO` for users model

---
+++ src/models/dto/users.dto.ts
@@ -0,0 +1,25 @@
+import IUser from "../../types/IUsers";
+
+class UsersDto implements IUser {
+  id: number;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  photo?: string;
+  bio: string;
+  resumeLink: string;
+
+  constructor(userObject: IUser) {
+    this.id = userObject.id;
+    this.email = userObject.email;
+    this.password = userObject.password;
+    this.firstName = userObject.firstName;
+    this.lastName = userObject.lastName;
+    this.photo = userObject.photo;
+    this.bio = userObject.bio;
+    this.resumeLink = userObject.resumeLink;
+  }
+}
+
+export default UsersDto;
