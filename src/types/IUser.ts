5ac2c227ba3025bb4ccaa66266d60936db18c5e7
Salma Hussien <salmaa.hussien1@gmail.com>
2024-03-15 23:41:09 +0200

    feat[backend]: implement user dto and interface

---
+++ src/types/IUser.ts
@@ -0,0 +1,12 @@
+interface IUser {
+  id?: number;
+  email?: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+  photo?: string;
+  bio?: string;
+  resumeLink?: string;
+}
+  
+export default IUser;
