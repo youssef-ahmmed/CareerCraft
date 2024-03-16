015278418c4710f55ee862e35782d8f53707b6bf
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-16 12:13:53 +0200

    feat[backend]: Implement Company interface

---
+++ src/types/ICompany.ts
@@ -0,0 +1,13 @@
+interface ICompany {
+  id?: number;
+  email?: string;
+  password?: string;
+  name?: string;
+  industry?: string;
+  location?: string;
+  logo?: string;
+  description?: string;
+  websiteLink?: string;
+}
+
+export default ICompany;
