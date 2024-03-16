9a6fe7c4092722897409fc0eda11e0e2f8fd99d6
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-16 12:10:06 +0200

    feat[backend]: Implement `DAO` for review model

---
+++ src/models/dao/review.dao.ts
@@ -0,0 +1,40 @@
+import prisma from './client.db';
+import IReview from "../../types/IReview";
+
+class ReviewDao {
+  static async createReview(reviewDto: any) {
+    return prisma.reviews.create({
+      data: reviewDto,
+    });
+  }
+
+  static async updateReview(reviewDto: IReview) {
+    return prisma.reviews.update({
+      where: { id: reviewDto.id },
+      data: reviewDto
+    });
+  }
+
+  static async getReviewById(reviewDto: IReview) {
+    return prisma.reviews.findUnique({
+      where: { id: reviewDto.id },
+    });
+  }
+
+  static async getReviewsByCompanyId(reviewDto: IReview) {
+    return prisma.reviews.findMany({
+      where: {
+        companyId: reviewDto.companyId,
+      }
+    });
+  }
+
+  static async deleteReviewById(reviewDto: IReview) {
+    return prisma.reviews.delete({
+      where: { id: reviewDto.id },
+    });
+  }
+
+}
+
+export default ReviewDao;
