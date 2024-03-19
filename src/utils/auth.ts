c2ee581a5870cf9ce3941546342040fd87c043cd
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-19 15:39:45 +0200

    refactor[middleware]: move `verifyToken` from utils to middlewares

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -11,33 +11,9 @@ export const comparePassword = (password: string, hash: string) => {
   return bcrypt.compare(password, hash);
 }
 
-export const createToken = (user: IUser) => {
-  return jwt.sign({ id: user.id },
+export const createToken = (id: number) => {
+  return jwt.sign({ id },
     process.env.TOKEN_SECRET,
     { expiresIn: '3d' }
   );
 };
-
-export const verifyToken = (req, res, next) => {
-  const bearer = req.headers.authorization;
-
-  if (!bearer) {
-    return res.status(401).json({ message: 'Not authorized' });
-  }
-
-  const [, token] = bearer.split(' ');
-
-  if (!token) {
-    return res.status(401).json({ message: 'There is no token' });
-  }
-
-  try {
-    const payload = jwt.verify(token, process.env.TOKEN_SECRET) as JwtPayload;
-    req.id = payload.id;
-
-    return next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Not a valid token' });
-  }
-}
-
