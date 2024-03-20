c7c8f729a0e274c53d769c72f1cf7291a512492f
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-20 02:47:16 +0200

    feat[util]: implement function to create token for sending email

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, {JwtPayload} from "jsonwebtoken";
 
 export const hashPassword = (password: string) => {
   return bcrypt.hash(password, Number(process.env.HASHING_SECRET));
@@ -13,5 +13,17 @@ export const createToken = (id: number) => {
   return jwt.sign({ id },
     process.env.TOKEN_SECRET,
     { expiresIn: '3d' }
+  );
+};
+
+export const verifyJwtToken = (token: string, secretKey: string) => {
+  return jwt.verify(token, secretKey) as JwtPayload;
+}
+
+export const createEmailToken = (user, secretKey: string) => {
+  return jwt.sign(
+    { id: user.id, email: user.email },
+    secretKey,
+    { expiresIn: '10m' }
   );
 };
