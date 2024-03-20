9f1308a993454828a3ebfe90514bfb8bc504bc26
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-20 18:11:55 +0200

    refactor[backend]: Rename user route and controller file

--- src/routes/user.auth.route.ts
+++ src/routes/user.auth.route.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
-import { validateUserEmailExistence, validateLoginUser, validateRegisterUser } from '../middlewares/user.validation';
+import {
+  validateUserEmailExistence,
+  validateLoginUser,
+  validateRegisterUser
+} from '../middlewares/user.validation';
 import UserAuthController from '../controllers/user.auth.controller';
+import uploadImage from '../middlewares/upload.image';
 
 const userRouter = Router();
 
 userRouter
-  .post('/signup', validateRegisterUser, validateUserEmailExistence, UserAuthController.register);
+  .post('/signup',
+    uploadImage.single('photo'),
+    validateRegisterUser,
+    validateUserEmailExistence,
+    UserAuthController.register);
 
 userRouter
   .post('/login', validateLoginUser, UserAuthController.login);
