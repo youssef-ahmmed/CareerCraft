8639029a3050f7c1461ade4b703e5faa419b0a70
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-20 14:19:43 +0200

    feat[middleware]: Implement validation middleware for company

---
+++ src/middlewares/company.validation.ts
@@ -0,0 +1,9 @@
+import { validateEmailExistence, validateReqBody } from './req.body.validation';
+import CompanyValidation from '../validations/company.validation';
+import CompanyDao from '../models/dao/company.dao';
+
+export const validateCompanyRegister = validateReqBody(CompanyValidation.createCompany);
+
+export const validateCompanyLogin = validateReqBody(CompanyValidation.login);
+
+export const validateCompanyEmailExistence = validateEmailExistence(CompanyDao.getCompanyByEmail);
