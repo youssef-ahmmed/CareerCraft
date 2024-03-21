c0bb6a242442a75e534a604aba8c6d9427a2f6f2
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-21 14:22:56 +0200

    feat[route]: Implement endpoints for company

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoute from './user.auth.route';
 import passwordRoute from './user.password.route';
 import companyAuthRouter from './company.auth.route';
 import companyPasswordRouter from './company.password.route';
+import companyRouter from './company.route';
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use('/users', passwordRoute);
 
 router.use('/companies', companyAuthRouter);
 router.use('/companies', companyPasswordRouter);
+router.use('/companies', companyRouter);
 
 export default router;
