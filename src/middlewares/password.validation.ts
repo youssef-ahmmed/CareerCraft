b8a0a83f07fe75bcc159d2744de46ad39d5029bb
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-21 07:43:27 +0200

    remove[middleware]: delete duplicate user and company password validation

--- src/middlewares/password.validation.ts
+++
@@ -1,8 +0,0 @@
-import UserValidation from "../validations/user.validation";
-import { validateReqBody } from "./req.body.validation";
-
-export const changePasswordValidator = validateReqBody(UserValidation.changePassword);
-
-export const forgetPasswordValidation = validateReqBody(UserValidation.forgetPassword);
-
-export const resetPasswordValidation = validateReqBody(UserValidation.resetPassword);
