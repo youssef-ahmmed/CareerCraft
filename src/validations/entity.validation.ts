9cb0585f6f57e3561dc7607eb0e4dc95f8d2d928
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-21 07:41:20 +0200

    refactor[validation]: move all validation schema of users and company to entity validation

---
+++ src/validations/entity.validation.ts
@@ -0,0 +1,74 @@
+import {Request} from "express";
+import joi from "joi";
+
+
+class EntityValidation {
+  static login (requestBody: Request) {
+    const schema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().min(6).required(),
+    });
+
+    return schema.validate(requestBody);
+  }
+
+  static createUser (requestBody: Request) {
+    const schema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().min(6).required(),
+      firstName: joi.string().trim().min(3).required(),
+      lastName: joi.string().trim().min(3).required(),
+      photo: joi.string().trim(),
+      bio: joi.string().min(5),
+      resumeLink: joi.string().required(),
+    });
+
+    return schema.validate(requestBody);
+  }
+
+  static updateUser (requestBody: Request) {
+    const schema = joi.object({
+      email: joi.string().email(),
+      password: joi.string().min(6),
+      firstName: joi.string().trim().min(3),
+      lastName: joi.string().trim().min(3),
+      photo: joi.string().trim(),
+      bio: joi.string().min(5),
+      resumeLink: joi.string(),
+    });
+
+    return schema.validate(requestBody);
+  }
+
+  static createCompany(requestBody: Request) {
+    const schema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().min(6).required(),
+      name: joi.string().trim().min(3).required(),
+      industry: joi.string().trim().min(3).required(),
+      location: joi.string().trim().min(3).required(),
+      logo: joi.string().trim().min(3).required(),
+      description: joi.string().trim().min(10).required(),
+      websiteLink: joi.string().trim().min(3).required(),
+    });
+
+    return schema.validate(requestBody);
+  }
+
+  static updateCompany(requestBody: Request) {
+    const schema = joi.object({
+      email: joi.string().email(),
+      password: joi.string().min(6),
+      name: joi.string().trim().min(3),
+      industry: joi.string().trim().min(3),
+      location: joi.string().trim().min(3),
+      logo: joi.string().trim().min(3),
+      description: joi.string().trim().min(10),
+      websiteLink: joi.string().trim().min(3),
+    });
+
+    return schema.validate(requestBody);
+  }
+}
+
+export default EntityValidation;
