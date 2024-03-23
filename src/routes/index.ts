9c5114ff7c4986179ba58d30fe19357561f50235
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-03-23 13:16:20 +0200

    feat[route]: add job router path in index file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import userPasswordRouter from './user.password.route';
 import companyAuthRouter from './company.auth.route';
 import companyPasswordRouter from './company.password.route';
 import companyRouter from './company.route';
+import jobRoute from "./job.route";
 
 const router = Router();
 
@@ -15,5 +16,7 @@ router.use('/users', userRouter);
 router.use('/companies', companyAuthRouter);
 router.use('/companies', companyPasswordRouter);
 router.use('/companies', companyRouter);
+
+router.use('/jobs', jobRoute);
 
 export default router;
