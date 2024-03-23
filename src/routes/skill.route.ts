f226a925a37efc15cae5e6826cc3e615d0d56f6a
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-23 10:47:03 +0200

    feat[route]: Implement endpoints for skill model

---
+++ src/routes/skill.route.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import { validateCreateSkill } from '../middlewares/skill.validation';
+import { verifyToken } from '../middlewares/verify.token';
+import SkillController from '../controllers/skill.controller';
+import {
+  verifyUserExistance,
+  verifyCompanyExistance,
+  verifyEntityExistance
+} from '../middlewares/verify.entity';
+
+
+const skillRouter = Router();
+
+skillRouter
+  .post('/users/skills',
+    verifyToken,
+    validateCreateSkill,
+    verifyUserExistance,
+    SkillController.creatSkillsByUser
+  );
+
+skillRouter
+  .delete('/users/skills/:skillId',
+    verifyToken,
+    verifyUserExistance,
+    SkillController.deleteSkillByUser
+  );
+
+skillRouter
+  .get('/users/:userId/skills',
+    verifyToken,
+    verifyEntityExistance,
+    SkillController.getSkillsByUserId
+  );
+
+skillRouter
+  .delete('/jobs/:jobId/skills/:skillId',
+    verifyToken,
+    verifyCompanyExistance,
+    SkillController.deleteSkillByJob
+  );
+
+export default skillRouter;
