3a939db02b144215d72a76b5e169c74c06333205
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-31 10:28:05 +0200

    refactor[validation]: Modify the constraints of the skills

--- src/validations/skill.validation.ts
+++ src/validations/skill.validation.ts
@@ -4,7 +4,7 @@ import { Request } from 'express';
 class SkillValidation {
   static createSkill(requestBody: Request) {
     const schema = joi.object({
-      skills: joi.array().items(joi.string().min(3).required()).required(),
+      skills: joi.array().items(joi.string().min(3).required()).min(1).required(),
     });
 
     return schema.validate(requestBody);
